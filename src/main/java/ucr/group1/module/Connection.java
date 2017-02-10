6778fa24749b54e448dec94ea613bd88c66d4bee
gonzo1805 <gonzo1805@gmail.com>
2017-02-09 22:54:16 -0600

    constructores de la abstracta

--- src/main/java/ucr/group1/module/Connection.java
+++ src/main/java/ucr/group1/module/Connection.java
@@ -2,15 +2,17 @@ package ucr.group1.module;
 
 import ucr.group1.query.Query;
 
+import java.awt.*;
+import java.util.Queue;
 import java.util.concurrent.locks.Condition;
 
 /**
  * Created by Gonzalo on 2/9/2017.
  */
-public class Connection extends Module<String> {
+public class Connection extends Module<Query> {
 
-    public Connection() {
-
+    public Connection(int numberOfFreeServers, int numberOfServers, Queue<Query> queue, Queue<Query> beignServedQuerys, Queue<Event> eventList) {
+        super(numberOfFreeServers, numberOfServers, queue, beignServedQuerys, eventList);
     }
 
     public void entriesANewQuery(Query query) {
--- src/main/java/ucr/group1/module/Module.java
+++ src/main/java/ucr/group1/module/Module.java
@@ -9,11 +9,19 @@ import ucr.group1.query.*;
  * Created by Gonzalo on 2/8/2017.
  */
 public abstract class Module<E> {
-    Queue<E> queue;
-    int numberOfServers;
-    int numberOfFreeServers;
-    Queue<E> beignServedQuerys;
-    Queue<Event/*TODO nuestra clase event*/> eventList;
+    protected Queue<E> queue;
+    protected int numberOfServers;
+    protected int numberOfFreeServers;
+    protected Queue<E> beignServedQuerys;
+    protected Queue<Event/*TODO nuestra clase event*/> eventList;
+
+    public Module(int numberOfFreeServers, int numberOfServers, Queue<E> queue, Queue<E> beignServedQuerys, Queue<Event> eventList) {
+        this.beignServedQuerys = beignServedQuerys;
+        this.queue = queue;
+        this.numberOfFreeServers = numberOfFreeServers;
+        this.numberOfServers = numberOfServers;
+        this.eventList = eventList;
+    }
 
     /**
      * Inserts a Query on queue if there is not free servers, if there are free servers, inserts a Query on
@@ -38,4 +46,23 @@ public abstract class Module<E> {
      */
     public abstract Query aQueryFinished();
 
+    public Queue<E> getQueue() {
+        return queue;
+    }
+
+    public int getNumberOfServers() {
+        return numberOfServers;
+    }
+
+    public int getNumberOfFreeServers() {
+        return numberOfFreeServers;
+    }
+
+    public Queue<E> getBeignServedQuerys() {
+        return beignServedQuerys;
+    }
+
+    public Queue<Event> getEventList() {
+        return eventList;
+    }
 }
