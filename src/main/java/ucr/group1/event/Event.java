3b9e172b90ad5897d5b63c450c7602cc2e3574a5
gonzo1805 <gonzo1805@gmail.com>
2017-02-11 11:57:36 -0600

    Clase Event lista

---
+++ src/main/java/ucr/group1/event/Event.java
@@ -0,0 +1,47 @@
+package ucr.group1.event;
+
+import ucr.group1.query.Query;
+
+/**
+ * Created by Gonzalo on 2/11/2017.
+ */
+public class Event {
+
+    private Query query;
+    private double time;
+    private eventType eventType;
+
+    public enum eventType {
+        ENTER_CONNECTION,
+        EXIT_CONNECTION,
+        ENTER_SYSTEMCALL,
+        EXIT_SYSTEMCALL,
+        ENTER_VALIDATION,
+        EXIT_VALIDATION,
+        ENTER_STORAGE,
+        EXIT_STORAGE,
+        ENTER_EXECUTION,
+        EXIT_EXECUTION,
+        KILL
+    }
+
+    public Query getQuery() {
+        return query;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public Event.eventType getEventType() {
+        return eventType;
+    }
+
+    public Event(eventType event, double time, Query associateQuery) {
+        this.eventType = event;
+        this.time = time;
+        this.query = associateQuery;
+    }
+
+
+}
