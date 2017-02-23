7af0fc4daba7847eadab0d7eb625569c92002e31
gonzo1805 <gonzo1805@gmail.com>
2017-02-22 18:30:56 -0600

    Integracion Velocity y agregado de una clase

--- src/main/java/ucr/group1/html/htmlGenerator.java
+++ src/main/java/ucr/group1/html/htmlGenerator.java
@@ -1,13 +1,32 @@
 package ucr.group1.html;
 
-import org.apache.velocity.app.*;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+
+import java.io.StringWriter;
 
 /**
  * Created by Gonzalo on 2/20/2017.
  */
 public class htmlGenerator {
 
-    public static void main(String[] args) {
+    public void crea() {
+         /*  first, get and initialize an engine  */
         VelocityEngine ve = new VelocityEngine();
+        ve.init();
+        /*  next, get the Template  */
+        Template t = ve.getTemplate("./src/main/resources/htmlGenerator.vcss");
+        /*  create a context and add data */
+        VelocityContext context = new VelocityContext();
+        context.put("nombre", "Gonzalo");
+        context.put("nombre2", "PERRO");
+        /* now render the template into a StringWriter */
+        StringWriter writer = new StringWriter();
+        t.merge(context, writer);
+        /* show the World */
+        System.out.println(writer.toString());
     }
+
+
 }
