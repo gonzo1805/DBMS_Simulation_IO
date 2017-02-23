c343e42b78c2d13381eeb1a0e779f1da7eea4a89
gonzo1805 <gonzo1805@gmail.com>
2017-02-23 01:01:47 -0600

    Conectadas la interfaz con la aplicacion, casi listo el generador de HTML

--- src/main/java/ucr/group1/html/htmlGenerator.java
+++ src/main/java/ucr/group1/html/htmlGenerator.java
@@ -4,7 +4,14 @@ import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.StringWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 /**
  * Created by Gonzalo on 2/20/2017.
@@ -19,13 +26,32 @@ public class htmlGenerator {
         Template t = ve.getTemplate("./src/main/resources/htmlGenerator.vcss");
         /*  create a context and add data */
         VelocityContext context = new VelocityContext();
-        context.put("nombre", "Gonzalo");
-        context.put("nombre2", "PERRO");
+        context.put("stabilityFirstModule", "Gonzalo");
+        context.put("avgRejectedConections", "PERRO");
+        context.put("newLine", "<li><a href=http://brackets.io>Brackets.io</a></li>");
         /* now render the template into a StringWriter */
         StringWriter writer = new StringWriter();
         t.merge(context, writer);
         /* show the World */
         System.out.println(writer.toString());
+        StringWriter finalWriter = new StringWriter();
+        ve.mergeTemplate("./src/main/resources/htmlGenerator.vcss", "utf-8", context, finalWriter);
+        String html = finalWriter.toString();
+
+
+        try {
+            File file = new File("./src/main/resources/index.html");
+            file.delete();
+            file.createNewFile();
+            Files.write(Paths.get("./src/main/resources/index.html"), html.getBytes(), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
